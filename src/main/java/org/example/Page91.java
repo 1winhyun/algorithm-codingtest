470bcdfb137f6b90475da80dac8063c17c2ce674
1winhyun <jimking1@naver.com>
2026-07-05 21:51:30 +0900

    feat: 카드 게임 (큐) (실버 4)

---
+++ src/main/java/org/example/Page91.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.*;
+
+public class Page91 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            int n = q.poll();
+            q.add(n);
+        }
+
+        System.out.println(q.peek());
+    }
+}
